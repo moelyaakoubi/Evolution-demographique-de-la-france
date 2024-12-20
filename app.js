39b0278f53636c578234b4170cbe3e96212441cd
mohammed el yaakoubi <medelyaakoubi365@gmail.com>
2024-12-20 13:08:23 +0100

    midified app

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'root',
+    user: 'user',
+    password: '',
     database: 'population_ville'
 });
 
